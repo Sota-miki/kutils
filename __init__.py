bb0cc24e1de1b2e9bef6ce0d890a59d789476ec5
Sota Miki <ghibli.totoro0317@gmail.com>
2024-11-20 01:59:17 +0900

    second

--- __init__.py
+++ __init__.py
@@ -1,22 +1,26 @@
 from . import generic, tensor_ops, image_utils
 from . import generators, model_helper, applications
 
-from keras import backend as K
-if K.backend()=='tensorflow': 
-    K.set_image_dim_ordering("tf")
+from tensorflow.keras import backend as K
+import tensorflow as tf
+
+if K.backend() == 'tensorflow': 
+    # TensorFlow 2.x ではチャンネルの順序はこのように設定します
+    tf.keras.backend.set_image_data_format("channels_last")  # "tf" に相当する設定
+
 
 # remove tensorflow warning
 import logging
 class WarningFilter(logging.Filter):
     def filter(self, record):
         msg = record.getMessage()
-        tf_warning = 'retry (from tensorflow.contrib.learn.python.learn.datasets.base)' in msg
+        tf_warning = 'retry' in msg
         return not tf_warning           
 logger = logging.getLogger('tensorflow')
 logger.addFilter(WarningFilter())
 
 # if too many warnings from scikit-image 
 import warnings
-warnings.filterwarnings("ignore")
+warnings.filterwarnings("ignore", category=UserWarning)
 
 print('loaded kutils')
